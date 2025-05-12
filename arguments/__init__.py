81e38e63ea53904671ca0fd6984574004ce7267c
SemyeongYu <semyu01@kaist.ac.kr>
2025-05-12 18:15:55 +0900

    Densification and convert_matrix_vector()

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -65,8 +65,8 @@ class ModelParams(ParamGroup):
 
 class PipelineParams(ParamGroup):
     def __init__(self, parser):
-        self.convert_SHs_python = False
-        self.compute_cov3D_python = False
+        self.convert_SHs_python = True
+        self.compute_cov3D_python = True
         self.debug = False
         self.antialiasing = False
         super().__init__(parser, "Pipeline Parameters")
--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -21,6 +21,7 @@ from utils.sh_utils import RGB2SH
 from simple_knn._C import distCUDA2
 from utils.graphics_utils import BasicPointCloud
 from utils.general_utils import strip_symmetric, build_scaling_rotation
+from scipy.spatial.transform import Rotation as R
 
 try:
     from diff_gaussian_rasterization import SparseGaussianAdam
@@ -207,6 +208,12 @@ class GaussianModel:
         rotation[:, :, 2] = rotation[:, :, 2] * det_R.sign().unsqueeze(-1)
 
         return scale, rotation # (N, 3, 3), (N, 3, 3)
+    
+    ### (N, 3, 3), (N, 3, 3) -> (N, 3), (N, 4)
+    def convert_matrix_vector(self, scale, rotation):
+        scale_vec = torch.stack([scale[:, 0, 0], scale[:, 1, 1], scale[:, 2, 2]], dim=1) # (N, 3)
+        rotation_vec = R.from_matrix(rotation).as_quat() # (N, 4)
+        return scale_vec, rotation_vec
 
     def create_from_pcd(self, pcd : BasicPointCloud, cam_infos : int, spatial_lr_scale : float):
         self.spatial_lr_scale = spatial_lr_scale
@@ -501,22 +508,22 @@ class GaussianModel:
         self.denom = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
         self.max_radii2D = torch.zeros((self.get_xyz.shape[0]), device="cuda")
 
-    def densify_and_split(self, grads, grad_threshold, scene_extent, N=2):
+    def densify_and_split(self, scale, grads, grad_threshold, scene_extent, N=2):
         n_init_points = self.get_xyz.shape[0]
         # Extract points that satisfy the gradient condition
         padded_grad = torch.zeros((n_init_points), device="cuda")
         padded_grad[:grads.shape[0]] = grads.squeeze()
         selected_pts_mask = torch.where(padded_grad >= grad_threshold, True, False)
         selected_pts_mask = torch.logical_and(selected_pts_mask,
-                                              torch.max(self.get_scaling, dim=1).values > self.percent_dense*scene_extent)
+                                              torch.max(scale, dim=1).values > self.percent_dense*scene_extent)
 
-        stds = self.get_scaling[selected_pts_mask].repeat(N,1)
+        stds = scale[selected_pts_mask].repeat(N,1)
         means =torch.zeros((stds.size(0), 3),device="cuda")
         samples = torch.normal(mean=means, std=stds)
         rots = build_rotation(self._rotation[selected_pts_mask]).repeat(N,1,1)
         new_xyz = torch.bmm(rots, samples.unsqueeze(-1)).squeeze(-1) + self.get_xyz[selected_pts_mask].repeat(N, 1)
         new_normal = self.get_normal[selected_pts_mask].repeat(N, 1) # do not change direction after densification
-        new_scaling = self.scaling_inverse_activation(self.get_scaling[selected_pts_mask].repeat(N,1) / (0.8*N))
+        new_scaling = self.scaling_inverse_activation(scale[selected_pts_mask].repeat(N,1) / (0.8*N))
         new_rotation = self._rotation[selected_pts_mask].repeat(N,1)
         new_features_dc = self._features_dc[selected_pts_mask].repeat(N,1,1)
         new_features_rest = self._features_rest[selected_pts_mask].repeat(N,1,1)
@@ -528,11 +535,11 @@ class GaussianModel:
         prune_filter = torch.cat((selected_pts_mask, torch.zeros(N * selected_pts_mask.sum(), device="cuda", dtype=bool))) # remove existing selected points and maintain new N * selected points
         self.prune_points(prune_filter)
 
-    def densify_and_clone(self, grads, grad_threshold, scene_extent):
+    def densify_and_clone(self, scale, grads, grad_threshold, scene_extent):
         # Extract points that satisfy the gradient condition
         selected_pts_mask = torch.where(torch.norm(grads, dim=-1) >= grad_threshold, True, False)
         selected_pts_mask = torch.logical_and(selected_pts_mask,
-                                              torch.max(self.get_scaling, dim=1).values <= self.percent_dense*scene_extent)
+                                              torch.max(scale, dim=1).values <= self.percent_dense*scene_extent)
         
         new_xyz = self._xyz[selected_pts_mask]
         new_normal = self._normal[selected_pts_mask]
@@ -547,17 +554,21 @@ class GaussianModel:
         self.densification_postfix(new_xyz, new_normal, new_features_dc, new_features_rest, new_opacities, new_scaling, new_rotation, new_tmp_radii)
 
     def densify_and_prune(self, max_grad, min_opacity, extent, max_screen_size, radii):
+        cov_cond = self.get_covariance(self.get_scaling, self.get_rotation)[0] # (N, 6)
+        scale_temp, rotation_temp = self.extract_SR(cov_cond) # (N, 3, 3), (N, 3, 3)
+        scale, _ = self.convert_matrix_vector(scale_temp, rotation_temp) # (N, 3), (N, 4)
+
         grads = self.xyz_gradient_accum / self.denom # do not reflect self.normal_gradient_accum into grads
         grads[grads.isnan()] = 0.0
 
         self.tmp_radii = radii
-        self.densify_and_clone(grads, max_grad, extent)
-        self.densify_and_split(grads, max_grad, extent)
+        self.densify_and_clone(scale, grads, max_grad, extent)
+        self.densify_and_split(scale, grads, max_grad, extent)
 
         prune_mask = (self.get_opacity < min_opacity).squeeze()
         if max_screen_size:
             big_points_vs = self.max_radii2D > max_screen_size
-            big_points_ws = self.get_scaling.max(dim=1).values > 0.1 * extent
+            big_points_ws = scale.max(dim=1).values > 0.1 * extent
             prune_mask = torch.logical_or(torch.logical_or(prune_mask, big_points_vs), big_points_ws)
         self.prune_points(prune_mask)
         tmp_radii = self.tmp_radii
