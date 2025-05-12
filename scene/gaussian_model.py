cc1912232d6209b11a9958ce68d55c8de5ba31d2
SemyeongYu <semyu01@kaist.ac.kr>
2025-05-12 18:42:27 +0900

    when split, reduce diag instead of scale

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -74,8 +74,8 @@ class GaussianModel:
         self._normal = torch.empty(0)
         self._features_dc = torch.empty(0)
         self._features_rest = torch.empty(0)
-        self._scaling = torch.empty(0)
-        self._rotation = torch.empty(0)
+        self._scaling = torch.empty(0) # diag
+        self._rotation = torch.empty(0) # offdiag
         self._opacity = torch.empty(0)
         self.max_radii2D = torch.empty(0)
         self.xyz_gradient_accum = torch.empty(0)
@@ -92,8 +92,8 @@ class GaussianModel:
             self._normal,
             self._features_dc,
             self._features_rest,
-            self._scaling,
-            self._rotation,
+            self._scaling, # diag
+            self._rotation, # offdiag
             self._opacity,
             self.max_radii2D,
             self.xyz_gradient_accum,
@@ -108,8 +108,8 @@ class GaussianModel:
         self._normal,
         self._features_dc, 
         self._features_rest,
-        self._scaling, 
-        self._rotation, 
+        self._scaling, # diag
+        self._rotation, # offdiag
         self._opacity,
         self.max_radii2D, 
         xyz_gradient_accum,
@@ -123,11 +123,11 @@ class GaussianModel:
     
     @property
     def get_scaling(self):
-        return self.scaling_activation(self._scaling)
+        return self.scaling_activation(self._scaling) # diag
     
     @property
     def get_rotation(self):
-        return self.rotation_activation(self._rotation)
+        return self.rotation_activation(self._rotation) # offdiag
     
     @property
     def get_xyz(self):
@@ -167,7 +167,7 @@ class GaussianModel:
     
     def get_covariance(self, scaling_modifier = 1):
         # return self.covariance_activation(self.get_scaling, scaling_modifier, self._rotation)
-        return self.covariance_activation(self.get_scaling, self.get_rotation) # 3D conditional covariance
+        return self.covariance_activation(self.get_scaling, self.get_rotation) # diag, offdiag -> 3D conditional covariance
 
     def oneupSHdegree(self):
         if self.active_sh_degree < self.max_sh_degree:
@@ -176,7 +176,7 @@ class GaussianModel:
     ### modified
     def slice_gaussian(self, d, lambda_opa=0.35):
         dim = 3
-        cov_cond, cov = self.get_covariance(self.get_scaling, self.get_rotation) # (N, 6), (N, 6, 6)
+        cov_cond, cov = self.get_covariance() # (N, 6), (N, 6, 6)
         cov_p = cov[:, :dim, :dim] # (N, 3, 3)
         cov_pd = cov[:, :dim, dim:] # (N, 3, 3)
         cov_d = cov[:, dim:, dim:] # (N, 3, 3)
@@ -325,9 +325,9 @@ class GaussianModel:
         for i in range(self._features_rest.shape[1]*self._features_rest.shape[2]):
             l.append('f_rest_{}'.format(i))
         l.append('opacity')
-        for i in range(self._scaling.shape[1]):
+        for i in range(self._scaling.shape[1]): # diag
             l.append('scale_{}'.format(i))
-        for i in range(self._rotation.shape[1]):
+        for i in range(self._rotation.shape[1]): # offdiag
             l.append('rot_{}'.format(i))
         return l
 
@@ -340,8 +340,8 @@ class GaussianModel:
         f_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
         f_rest = self._features_rest.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
         opacities = self._opacity.detach().cpu().numpy()
-        scale = self._scaling.detach().cpu().numpy()
-        rotation = self._rotation.detach().cpu().numpy()
+        scale = self._scaling.detach().cpu().numpy() # diag
+        rotation = self._rotation.detach().cpu().numpy() # offdiag
 
         dtype_full = [(attribute, 'f4') for attribute in self.construct_list_of_attributes()]
 
@@ -454,8 +454,8 @@ class GaussianModel:
         self._features_dc = optimizable_tensors["f_dc"]
         self._features_rest = optimizable_tensors["f_rest"]
         self._opacity = optimizable_tensors["opacity"]
-        self._scaling = optimizable_tensors["scaling"]
-        self._rotation = optimizable_tensors["rotation"]
+        self._scaling = optimizable_tensors["scaling"] # diag
+        self._rotation = optimizable_tensors["rotation"] # offdiag
 
         self.xyz_gradient_accum = self.xyz_gradient_accum[valid_points_mask]
 
@@ -500,15 +500,15 @@ class GaussianModel:
         self._features_dc = optimizable_tensors["f_dc"]
         self._features_rest = optimizable_tensors["f_rest"]
         self._opacity = optimizable_tensors["opacity"]
-        self._scaling = optimizable_tensors["scaling"]
-        self._rotation = optimizable_tensors["rotation"]
+        self._scaling = optimizable_tensors["scaling"] # diag
+        self._rotation = optimizable_tensors["rotation"] # offdiag
 
         self.tmp_radii = torch.cat((self.tmp_radii, new_tmp_radii))
         self.xyz_gradient_accum = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
         self.denom = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
         self.max_radii2D = torch.zeros((self.get_xyz.shape[0]), device="cuda")
 
-    def densify_and_split(self, scale, grads, grad_threshold, scene_extent, N=2):
+    def densify_and_split(self, scale, rotation, grads, grad_threshold, scene_extent, N=2):
         n_init_points = self.get_xyz.shape[0]
         # Extract points that satisfy the gradient condition
         padded_grad = torch.zeros((n_init_points), device="cuda")
@@ -520,11 +520,11 @@ class GaussianModel:
         stds = scale[selected_pts_mask].repeat(N,1)
         means =torch.zeros((stds.size(0), 3),device="cuda")
         samples = torch.normal(mean=means, std=stds)
-        rots = build_rotation(self._rotation[selected_pts_mask]).repeat(N,1,1)
+        rots = build_rotation(rotation[selected_pts_mask]).repeat(N,1,1)
         new_xyz = torch.bmm(rots, samples.unsqueeze(-1)).squeeze(-1) + self.get_xyz[selected_pts_mask].repeat(N, 1)
         new_normal = self.get_normal[selected_pts_mask].repeat(N, 1) # do not change direction after densification
-        new_scaling = self.scaling_inverse_activation(scale[selected_pts_mask].repeat(N,1) / (0.8*N))
-        new_rotation = self._rotation[selected_pts_mask].repeat(N,1)
+        new_scaling = self.scaling_inverse_activation(self.get_scaling[selected_pts_mask].repeat(N,1) / (0.8*N)) # scale for scale, self._scaling for diag
+        new_rotation = self._rotation[selected_pts_mask].repeat(N,1) # offdiag
         new_features_dc = self._features_dc[selected_pts_mask].repeat(N,1,1)
         new_features_rest = self._features_rest[selected_pts_mask].repeat(N,1,1)
         new_opacity = self._opacity[selected_pts_mask].repeat(N,1)
@@ -535,7 +535,7 @@ class GaussianModel:
         prune_filter = torch.cat((selected_pts_mask, torch.zeros(N * selected_pts_mask.sum(), device="cuda", dtype=bool))) # remove existing selected points and maintain new N * selected points
         self.prune_points(prune_filter)
 
-    def densify_and_clone(self, scale, grads, grad_threshold, scene_extent):
+    def densify_and_clone(self, scale, rotation, grads, grad_threshold, scene_extent):
         # Extract points that satisfy the gradient condition
         selected_pts_mask = torch.where(torch.norm(grads, dim=-1) >= grad_threshold, True, False)
         selected_pts_mask = torch.logical_and(selected_pts_mask,
@@ -546,24 +546,24 @@ class GaussianModel:
         new_features_dc = self._features_dc[selected_pts_mask]
         new_features_rest = self._features_rest[selected_pts_mask]
         new_opacities = self._opacity[selected_pts_mask]
-        new_scaling = self._scaling[selected_pts_mask]
-        new_rotation = self._rotation[selected_pts_mask]
+        new_scaling = self._scaling[selected_pts_mask] # diag
+        new_rotation = self._rotation[selected_pts_mask] # offdiag
 
         new_tmp_radii = self.tmp_radii[selected_pts_mask]
 
         self.densification_postfix(new_xyz, new_normal, new_features_dc, new_features_rest, new_opacities, new_scaling, new_rotation, new_tmp_radii)
 
     def densify_and_prune(self, max_grad, min_opacity, extent, max_screen_size, radii):
-        cov_cond = self.get_covariance(self.get_scaling, self.get_rotation)[0] # (N, 6)
-        scale_temp, rotation_temp = self.extract_SR(cov_cond) # (N, 3, 3), (N, 3, 3)
-        scale, _ = self.convert_matrix_vector(scale_temp, rotation_temp) # (N, 3), (N, 4)
+        cov_cond = self.get_covariance()[0] # (N, 6)
+        scale_mat, rotation_mat = self.extract_SR(cov_cond) # (N, 3, 3), (N, 3, 3)
+        scale, rotation = self.convert_matrix_vector(scale_mat, rotation_mat) # (N, 3), (N, 4)
 
         grads = self.xyz_gradient_accum / self.denom # do not reflect self.normal_gradient_accum into grads
         grads[grads.isnan()] = 0.0
 
         self.tmp_radii = radii
-        self.densify_and_clone(scale, grads, max_grad, extent)
-        self.densify_and_split(scale, grads, max_grad, extent)
+        self.densify_and_clone(scale, rotation, grads, max_grad, extent)
+        self.densify_and_split(scale, rotation, grads, max_grad, extent)
 
         prune_mask = (self.get_opacity < min_opacity).squeeze()
         if max_screen_size:
