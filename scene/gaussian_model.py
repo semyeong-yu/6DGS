0603d7b42a2776903f13d11b760c302957a661b1
SemyeongYu <semyu01@kaist.ac.kr>
2025-05-27 07:30:52 +0000

    0527

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -395,7 +395,7 @@ class GaussianModel:
         PlyData([el]).write(path)
 
     def reset_opacity(self):
-        opacities_new = self.inverse_opacity_activation(torch.min(self.get_opacity, torch.ones_like(self.get_opacity)*0.01))
+        opacities_new = self.inverse_opacity_activation(torch.min(self.get_opacity, torch.ones_like(self.get_opacity)*0.5)) # 0.01
         optimizable_tensors = self.replace_tensor_to_optimizer(opacities_new, "opacity")
         self._opacity = optimizable_tensors["opacity"]
 
--- train.py
+++ train.py
@@ -171,7 +171,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                     size_threshold = 20 if iteration > opt.opacity_reset_interval else None
                     gaussians.densify_and_prune(opt.densify_grad_threshold, 0.01, scene.cameras_extent, size_threshold, radii)
                 
-                if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
+                # if iteration % opt.opacity_reset_interval == 0 or (dataset.white_background and iteration == opt.densify_from_iter):
+                if dataset.white_background and iteration == opt.densify_from_iter: # 6DGS
                     gaussians.reset_opacity()
 
             # Optimizer step
